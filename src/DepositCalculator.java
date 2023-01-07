1d2ce05f8851ed8035ac5825796c11c26e86a16f
Aleksey Karev <laurent.94@yandex.ru>
2023-01-07 18:38:01 +0300

    Changed code style of 'calculate_deposit' class

---
+++ src/DepositCalculator.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class DepositCalculator {
+    double calculateComplexPercent(double amount, double yearRate, int depositPeriod) {
+        double pay = amount * Math.pow((1 + yearRate / 12), 12 * depositPeriod);
+        return round(pay, 2);
+    }
+
+    double calculateSimplePercent(double amount, double yearRate, int depositPeriod) {
+        return round(amount + amount * yearRate * depositPeriod, 2);
+    }
+
+    double round(double value, int places) {
+        double scale = Math.pow(10, places);
+        return Math.round(value * scale) / scale;
+    }
+
+    void calculateDepositIncome() {
+        int amount;
+        int period;
+        int depositType;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите сумму вклада в рублях:");
+        amount = scanner.nextInt();
+        System.out.println("Введите срок вклада в годах:");
+        period = scanner.nextInt();
+        System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
+        depositType = scanner.nextInt();
+
+        double income = 0;
+
+        if (depositType == 1) {
+            income = calculateSimplePercent(amount, 0.06, period);
+        } else if (depositType == 2) {
+            income = calculateComplexPercent(amount, 0.06, period);
+        }
+        System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + income);
+    }
+
+    public static void main(String[] args) {
+        new DepositCalculator().calculateDepositIncome();
+    }
+}
